8411a7fa86e1fabeafee4683039af4a72d21b563
Marc Lelijveld <Marc.lelijveld@outlook.com>
2022-06-08 14:34:51 +0200

    Decks from end of May and beginning of June

---
+++ 2022/2022-06-03_Data Grillen - Lingen/PBI Run Backup operation.ps1
@@ -0,0 +1,32 @@
+﻿# Run parameters, please specify below parameters
+$WorkspaceName = "SQL Bits" # Here specify the workspace name, not the id! Please replace spaces for %20. This is used to concatenate the XMLA endpoint later. 
+$DatasetName = "Manual setup partitions" # DatasetName to find the dataset and later to be used in backup filename
+
+# Base variables
+$PbiBaseConnection = "powerbi://api.powerbi.com/v1.0/myorg/"
+$XmlaEndpoint = $PbiBaseConnection + $WorkspaceName
+
+$NamePrefix = Get-Date -Format "yyyyMMdd-HHmmss" #Gets Date and Time on which the backup is performed as prefix to list backups easily in order
+$BackupFileName = $NamePrefix + "_" + $DatasetName
+
+# TMSL Script for backup
+$TmslScript = 
+@"
+{
+  "backup": {
+    "database": "$DatasetName",
+    "file": "$BackupFileName.abf",
+    "allowOverwrite": false,
+    "applyCompression": true
+  }
+}
+"@
+
+# Execute backup operation
+Try {
+    Invoke-ASCmd -Query $TmslScript -Server: $XmlaEndpoint
+}
+Catch{
+    # Write message if error
+    Write-Host "An error occured" -ForegroundColor Red
+}
