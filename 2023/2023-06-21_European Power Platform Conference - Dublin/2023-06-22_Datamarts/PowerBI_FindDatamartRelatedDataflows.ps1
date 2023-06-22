678c4c4952444bae101fb3b9c8d42750370367a8
Marc Lelijveld <Marc.lelijveld@outlook.com>
2023-06-22 11:53:10 +0200

    EPPC '23 - Dublin

---
+++ 2023/2023-06-21_European Power Platform Conference - Dublin/2023-06-22_Datamarts/PowerBI_FindDatamartRelatedDataflows.ps1
@@ -0,0 +1,13 @@
+﻿# Define workspace Id
+$WorkspaceId = "9d371bd6-0afc-4837-a69c-ff7f26a3bb97"
+
+# Sign in to Power BI Service
+Connect-PowerBIServiceAccount 
+
+# Get all dataflows in workspace
+$ListDataflows = Invoke-PowerBIRestMethod -Method GET -Url https://api.powerbi.com/v1.0/myorg/groups/$WorkspaceId/dataflows | ConvertFrom-Json
+$ListDataflows.value
+
+# Get dataflow source
+$DataFlowObjectId = $ListDataflows.value.objectId
+Invoke-PowerBIRestMethod -Method GET -Url https://api.powerbi.com/v1.0/myorg/groups/$WorkspaceId/dataflows/$DataFlowObjectId/datasources
